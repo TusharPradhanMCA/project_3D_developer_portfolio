7cede4baae9513c13ed2066db5397be70572c377
TushanPradhan <tushar@orufy.com>
2025-07-25 21:31:58 +0530

    logo fix for mobile view

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { isMobile } from "react-device-detect";
 
 import { BallCanvas } from "./canvas";
 import { SectionWrapper } from "../hoc";
@@ -9,7 +10,15 @@ const Tech = () => {
     <div className='flex flex-row flex-wrap justify-center gap-10'>
       {technologies.map((technology) => (
         <div className='w-28 h-28' key={technology.name}>
-          <BallCanvas icon={technology.icon} />
+          {isMobile ? (
+            <img
+              src={technology.icon}
+              alt={technology.name}
+              className='w-full h-full object-contain'
+            />
+          ) : (
+            <BallCanvas icon={technology.icon} />
+          )}
         </div>
       ))}
     </div>
