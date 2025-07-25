8c54a8393d5058962b6f29d8ce3aa55f097eb4d9
TushanPradhan <tushar@orufy.com>
2025-07-25 22:43:26 +0530

    change

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,46 +6,40 @@ import { ComputersCanvas } from "./canvas";
 
 const Hero = () => {
   return (
-    <section className="relative w-full h-screen mx-auto">
+    <section className='relative w-full h-screen mx-auto'>
       {/* Text and dots */}
       <div
         className={`absolute inset-0 top-[120px] max-w-7xl mx-auto ${styles.paddingX} flex flex-row items-start gap-5`}
       >
-        <div className="flex flex-col justify-center items-center mt-5">
-          <div className="w-5 h-5 rounded-full bg-[#915EFF]" />
-          <div className="w-1 sm:h-80 h-40 violet-gradient" />
+        <div className='flex flex-col justify-center items-center mt-5'>
+          <div className='w-5 h-5 rounded-full bg-[#915EFF]' />
+          <div className='w-1 sm:h-80 h-40 violet-gradient' />
         </div>
 
         <div>
           <h1 className={`${styles.heroHeadText} text-white`}>
-            Hi, I'm <span className="text-[#915EFF]">TUSHAR</span>
+            Hi, I'm <span className='text-[#915EFF]'>TUSHAR</span>
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
-            Welcome to my portfolio site <br className="sm:block hidden" />
+            Welcome to my portfolio site <br className='sm:block hidden' />
             I’m a Software Test Engineer.
           </p>
         </div>
       </div>
 
-      {/* Image or 3D Canvas */}
       {isMobile ? (
         <img
-          src="/fallback-setup.png"
-          alt="developer setup"
-          className="absolute bottom-0 w-full h-[450px] object-contain"
+          src='/fallback-setup.png'
+          alt='developer setup'
+          className='absolute bottom-0 w-full h-[450px] object-contain'
         />
       ) : (
         <ComputersCanvas />
       )}
 
-      {/* Scroll Button */}
-      <div
-        className={`w-full flex justify-center items-center ${
-          isMobile ? "relative mt-10" : "absolute bottom-5"
-        }`}
-      >
-        <a href="#about">
-          <div className="w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2">
+      <div className='absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center'>
+        <a href='#about'>
+          <div className='w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2'>
             <motion.div
               animate={{ y: [0, 24, 0] }}
               transition={{
@@ -53,7 +47,7 @@ const Hero = () => {
                 repeat: Infinity,
                 repeatType: "loop",
               }}
-              className="w-3 h-3 rounded-full bg-secondary mb-1"
+              className='w-3 h-3 rounded-full bg-secondary mb-1'
             />
           </div>
         </a>
